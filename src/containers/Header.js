87b251043b898c47ba69764d135a2a4b75ae080d
Ananta Sontakke <92220773+Ananta-01@users.noreply.github.com>
2022-01-22 17:13:13 +0530

    Add files via upload

---
+++ src/containers/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container center">
+      <div class="ui secondary pointing menu">
+      <div class="active item">Fake Api Store</div>
+      <a class="item">About</a>
+      <a class="item">Contact us</a>
+    </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
